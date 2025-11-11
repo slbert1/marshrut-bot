ea1818d0ffac5e5af91223b773787f243e4db00f
slbert1 <66589589+slbert1@users.noreply.github.com>
2025-11-11 20:36:13 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -183,7 +183,7 @@ async def main():
         try:
             await dp.start_polling(bot)
         except Exception as e:
-            print(f"[ОШИБКА] {e}")
+            print(f"[ОШИБКА] {e} — переподключение через 5 сек...")
             await asyncio.sleep(5)
 
 if __name__ == '__main__':
